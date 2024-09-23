e5409eb1dcf0c4eef69c2c5e8299476c5b236fb8
KalilRamos <134665832+KalilRamos@users.noreply.github.com>
2024-09-23 20:29:22 -0300

    Update Prototype2.ino

--- Prototype2.ino
+++ Prototype2.ino
@@ -1,16 +1,16 @@
-#include <DHT.h>
+#include <DHT.h> // Inclui biblioteca para o funcionamento do sensor
 
-#include "thingProperties.h"
+#include "thingProperties.h" // Inclui a biblioteca para que a integração do código com a plataforma funcione
 const int DHTPIN = 32;
 const int DHTTYPE = DHT11;
 float temperatura = 0;
 
-DHT dht(DHTPIN, DHTTYPE);
+DHT dht(DHTPIN, DHTTYPE); // Define o tipo do sensor DHT11
 
 void setup() {
   // Inicia o monitor serial
   Serial.begin(9600);
-  delay(1500); 
+  delay(1500); // Intervalo de 1 segundo e meio entre as impressões no monitor serial
 
   // Defined in thingProperties.h
   initProperties();
@@ -25,20 +25,22 @@ void setup() {
 }
 // Inicia a leitura da temperatura
 float lerTemperatura() {
-  float temperature = dht.readTemperature();
-  return temperature;
+  float temperature = dht.readTemperature(); // Sensor lê a temperatura
+  return temperature; // Sernsor devolve a temperatura obtida
 }
 
 void loop() {
   // atualiza os dados no cloud
   ArduinoCloud.update();
-  delay(1000);
+  delay(1000); // Intervalo de 1 segundo para a atualização na plataforma de monitoramento
   // Chama a variável criada no cloud para o envio das leituras do sensor
   temperatura = lerTemperatura();
   tempH = temperatura;
   // Imprime a temperatura
   Serial.print("Temperature (C): ");
   Serial.println(temperatura);
+
+  delay(1000); // Intervalo de 1 segundo para o loop do código
   
   
 }
